b9308e2dee853127f126ae9d3248339d710f8bf9
Matt Im <matthew.seiim@gmail.com>
2020-05-08 15:47:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,13 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def initialize
+    self.create
+    @name = name
+    @artist_name = artist_name
+    @@all
+  end
+
   def self.all
     @@all
   end
