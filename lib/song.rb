b78f2037a068748c8d3ca4f55ec85615ff94c2e0
Matt Im <matthew.seiim@gmail.com>
2020-05-08 16:01:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,18 +5,16 @@ class Song
   def self.all
     @@all
   end
-  
-  def initialize(name, artist_name)
-    @name = name
-    @artist_name = artist_name
-    @@all << self
-  end
+
 
   def save
     self.class.all << self
   end
 
-  def Song.create
-    save self.class.all
+  def self.create
+    song = self.new 
+    self.all << song 
+    song
+  end
 
-end
+  
